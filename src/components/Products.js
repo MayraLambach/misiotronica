bd4879aa3e94d2903521b1686a019086ab9ea836
MayraLambach <mayralambach@hotmail.com>
2023-08-20 07:59:55 -0300

    agregue info en about us

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Card from './card/Card'
 import './products.css'
 
@@ -9,53 +8,132 @@ import product4 from '../assets/luz led wifi.jpg'
 import product5 from '../assets/placa de sonido.jpg' 
 import product6 from '../assets/placa sonido externa.jpg' 
 
+/*
+const products = [{
+    image:{product1},
+    title:"Cable HDMI",
+    description:"Este cable es ideal para conectar tus dispositivos de manera fácil y sencilla. Podrás transferir tu audio y video."
+    },{
+    image:{product2},
+    title:"Cable USB tipo C",
+    description:"Compatible con carga rápida, lo que posibilita usarlo con la mayoría de los cargadores rápidos del mercado."
+    },{
+    image:{product3},
+    title:"Extensor wifi",
+    description:"Amplifica la señal de tu Internet inalámbrico por hasta 40m."
+    },{
+    image:{product4},
+    title:"Foco inteligente",
+    description:"Este foco compatible con Wi-Fi puede ser una excelente opción si buscás automatizar el encendido y apagado desde aplicaciones del teléfono y desde un asistente virtual."
+    },{
+    image:{product5},
+    title:"Placa de sonido PCI",
+    description:"La placa de sonido te te asegura trasladar a tu puerto PCI EXPRESS la mejor calidad de audio con sonido envolvente."
+    },{
+    image:{product6},
+    title:"Placa de sonido externa",
+    description:"Con esta placa externa tendrás audio Surround y 3D mediante tu puerto USB, es Plug and Play por lo que no necesitarás instalar drivers adicionales una vez conectado. "
+    }
+]
+
+*/
+
 function Products() {
     return (
         <div className='products'>
-            <div>
+            <div className='product' id='product-1'>
                 <Card
-                image={product1}
-                title="Cable HDMI"
-                description="Este cable es ideal para conectar tus dispositivos de manera fácil y sencilla. Podrás transferir tu audio y video."
+                    image={product1}
+                    title="Cable HDMI"
+                    description="Este cable es ideal para conectar tus dispositivos de manera fácil y sencilla. Podrás transferir tu audio y video."
                 />
             </div>
-            <div>
-            <Card
-                image={product2}
-                title="Cable USB tipo C"
-                description="Compatible con carga rápida, lo que posibilita usarlo con la mayoría de los cargadores rápidos del mercado."
+            <div className='arrows'>
+                <a href="#product-6">
+                    <i className="fa-sharp fa-solid fa-arrow-left fa-beat-fade fa-2xs"></i>
+                </a>
+                <a href="#product-2">
+                    <i className="fa-sharp fa-solid fa-arrow-right fa-beat-fade fa-2xs"></i>
+                </a>
+            </div>
+            <div className='product' id='product-2'>
+                <Card
+                    image={product2}
+                    title="Cable USB tipo C"
+                    description="Compatible con carga rápida, lo que posibilita usarlo con la mayoría de los cargadores rápidos del mercado."
                 />
             </div>
-            <div>
+            <div className='arrows'>
+                <a href="#product-1">
+                    <i className="fa-sharp fa-solid fa-arrow-left fa-beat-fade fa-2xs"></i>
+                </a>
+                <a href="#product-3">
+                    <i className="fa-sharp fa-solid fa-arrow-right fa-beat-fade fa-2xs"></i>
+                </a>
+            </div>
+            <div className='product' id='product-3'>
                 <Card
-                image={product3}
-                title="Extensor wifi"
-                description="Amplifica la señal de tu Internet inalámbrico por hasta 40m."
+                    image={product3}
+                    title="Extensor wifi"
+                    description="Amplifica la señal de tu Internet inalámbrico por hasta 40m."
                 />
             </div>
-            <div>
+            <div className='arrows'>
+                <a href="#product-2">
+                    <i className="fa-sharp fa-solid fa-arrow-left fa-beat-fade fa-2xs"></i>
+                </a>
+                <a href="#product-4">
+                    <i className="fa-sharp fa-solid fa-arrow-right fa-beat-fade fa-2xs"></i>
+                </a>
+            </div>
+            <div className='product' id='product-4'>
                 <Card
-                image={product4}
-                title="Foco inteligente"
-                description="Este foco compatible con Wi-Fi puede ser una excelente opción si buscás automatizar el encendido y apagado desde aplicaciones del teléfono y desde un asistente virtual."
+                    image={product4}
+                    title="Foco inteligente"
+                    description="Este foco compatible con Wi-Fi puede ser una excelente opción si buscás automatizar el encendido y apagado desde aplicaciones del teléfono y desde un asistente virtual."
                 />
             </div>
-            <div>
+            <div className='arrows'>
+                <a href="#product-3">
+                    <i className="fa-sharp fa-solid fa-arrow-left fa-beat-fade fa-2xs"></i>
+                </a>
+                <a href="#product-5">
+                    <i className="fa-sharp fa-solid fa-arrow-right fa-beat-fade fa-2xs"></i>
+                </a>
+            </div>
+            <div className='product' id='product-5'>           
                 <Card
-                image={product5}
-                title="Placa de sonido PCI"
-                description="La placa de sonido te te asegura trasladar a tu puerto PCI EXPRESS la mejor calidad de audio con sonido envolvente."
+                    image={product5}
+                    title="Placa de sonido PCI"
+                    description="La placa de sonido te te asegura trasladar a tu puerto PCI EXPRESS la mejor calidad de audio con sonido envolvente."
                 />
             </div>
-            <div>
+            <div className='arrows'>
+                <a href="#product-4">
+                    <i className="fa-sharp fa-solid fa-arrow-left fa-beat-fade fa-2xs"></i>
+                </a>
+                <a href="#product-6">
+                    <i className="fa-sharp fa-solid fa-arrow-right fa-beat-fade fa-2xs"></i>
+                </a>
+            </div>
+            <div className='product' id='product-6'>
                 <Card
-                image={product6}
-                title="Placa de sonido externa"
-                description="Con esta placa externa tendrás audio Surround y 3D mediante tu puerto USB, es Plug and Play por lo que no necesitarás instalar drivers adicionales una vez conectado. "
+                    image={product6}
+                    title="Placa de sonido externa"
+                    description="Con esta placa externa tendrás audio Surround y 3D mediante tu puerto USB, es Plug and Play por lo que no necesitarás instalar drivers adicionales una vez conectado. "
                 />
+            </div>
+            <div className='arrows'>
+                <a href="#product-5">
+                    <i className="fa-sharp fa-solid fa-arrow-left fa-beat-fade fa-2xs"></i>
+                </a>
+                <a href="#product-1">
+                    <i className="fa-sharp fa-solid fa-arrow-right fa-beat-fade fa-2xs"></i>
+                </a>
             </div>
         </div>
     )
 }
+
 
 export default Products
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,8 @@ const About = () => {
     return (
         <div className="about">
             <h1>Sobre nosotros</h1>
-            <p>Somos una PYME que vende elementos electrónicos con más de 5 años de trayectoria. Encontrá lo que buscas y lo que no sabías que necesitabas.</p>
+            <p>Somos una PYME que vende elementos electrónicos con más de 5 años de trayectoria. Desde nuestros inicios nos hemos enfocado en conocer, interpretar y ofrecer las mejores soluciones para las necesidades de nuestros clientes. Por ello nuestra meta es estar a la vanguardia en los insumos tecnológicos que el mercado exige, para acompañarte y hacer de la tecnología tu herramienta aliada.</p>
+            <p>Encontrá lo que buscas y lo que no sabías que necesitabas.</p>
         </div>
     )
 }
